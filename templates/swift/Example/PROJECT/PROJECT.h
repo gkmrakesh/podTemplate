1074d324dd8eb859f36eb21c9b31c8111bbddd84
Rakesh MEMBARAM <rakesh.membaram@amadeus.com>
2017-12-01 19:57:35 +0530

    changed example project

---
+++ templates/swift/Example/PROJECT/PROJECT.h
@@ -0,0 +1,19 @@
+//
+//  PROJECT.h
+//  PROJECT
+//
+//  Created by Rakesh MEMBARAM on 1/12/17.
+//  Copyright © 2017 Rakesh MEMBARAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PROJECT.
+FOUNDATION_EXPORT double PROJECTVersionNumber;
+
+//! Project version string for PROJECT.
+FOUNDATION_EXPORT const unsigned char PROJECTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PROJECT/PublicHeader.h>
+
+
---
+++ templates/swift/Example/Pods/Target Support Files/Pods-PROJECT/Pods-PROJECT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PROJECT : NSObject
+@end
+@implementation PodsDummy_Pods_PROJECT
+@end
